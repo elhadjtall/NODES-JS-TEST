9dbe5c6845c32c2feb9e10f9229b88cf1e0492af
Elhadj Tall <elhadjoumartall10@gmail.com>
2025-03-11 11:52:39 +0100

    Initialisation du projet

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express'); // Importation du framework Express
-const app = express(); // Création de l'application Express
+import express from 'express';
 
-const PORT = 3000; // Définition du port
+const app = express();
 
-// Définition d'une route pour afficher "Hello, World!"
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-// Lancement du serveur
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+    console.log(`Serveur démarré sur le port ${PORT}`);
 });
+
+// Ajoute cette ligne pour exporter `app`
+export default app;
--- test/test.js
+++ test/test.js
@@ -1,16 +1,12 @@
-const request = require('supertest');
-const chai = require('chai');
-const expect = chai.expect;
-const app = require('../app');
+import request from 'supertest';
+import { expect } from 'chai';
+import app from '../server.js'; // Assure-toi que server.js exporte `app`
+
 
 describe('Test de la route principale', () => {
-    it('devrait retourner Hello, World!', (done) => {
-        request(app)
-            .get('/')
-            .end((err, res) => {
-                expect(res.status).to.equal(200);
-                expect(res.text).to.equal('Hello, World!');
-                done();
-            });
+    it('devrait retourner Hello, World!', async () => {
+        const res = await request(app).get('/');
+        expect(res.status).to.equal(200);
+        expect(res.text).to.equal('Hello, World!');
     });
 });
